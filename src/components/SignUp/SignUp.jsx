001a490d9d6ef3857a709722e3fadaeceda88f1d
Joyeta-Mondal <dipannitakotha2019@gmail.com>
2025-01-11 02:21:32 +0530

    added error msg for terms and cond

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,15 +1,19 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase.init";
 import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const SignUp = () => {
   const [successMsg, setSuccessMsg] = useState(false);
   const [error, setError] = useState("");
+  const [showPassEye, setShowPassEye] = useState(false);
+
   const handleSignUp = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
     const username = e.target.username.value;
+    const terms = e.target.terms.checked;
 
     setError("");
     setSuccessMsg(false);
@@ -18,6 +22,20 @@ const SignUp = () => {
       setError("Password should be at least of 6 characters.");
       return;
     }
+
+    if (!terms) {
+      setError("Please accept out terms and conditions to sign up.");
+      return;
+    }
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{6,}$/;
+    if (!passwordRegex.test(password)) {
+      setError(
+        "Password should have at least one uppercase, one lowercase, one number, and one special character."
+      );
+      return;
+    }
+
     createUserWithEmailAndPassword(auth, email, password, username)
       .then((result) => {
         console.log(result.user);
@@ -28,6 +46,7 @@ const SignUp = () => {
         setSuccessMsg(false);
       });
   };
+
   return (
     <div className="card bg-base-100 w-full max-w-sm mx-auto my-12 shrink-0 shadow-2xl">
       <h1 className="text-3xl font-bold text-center">Sign Up now!</h1>
@@ -60,21 +79,39 @@ const SignUp = () => {
             required
           />
         </div>
-        <div className="form-control">
+        <div className="form-control relative">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
           <input
-            type="password"
+            type={showPassEye ? "text" : "password"}
             name="password"
             placeholder="password"
             className="input input-bordered"
             required
           />
+          <button
+            onClick={() => setShowPassEye(!showPassEye)}
+            className="btn btn-xs absolute right-2 top-12"
+          >
+            {showPassEye ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
+          </button>
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">
               Forgot password?
             </a>
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label justify-start gap-3 cursor-pointer">
+            <input
+              type="checkbox"
+              name="terms"
+              className="checkbox checkbox-primary"
+            />
+            <span className="label-text">
+              Accept our terms and conditions to sign up
+            </span>
           </label>
         </div>
         <div className="form-control mt-6">
@@ -82,7 +119,7 @@ const SignUp = () => {
         </div>
       </form>
       {error && <p className="text-red-700">{error}</p>}
-      {successMsg && <p className="text-green-700">Successfully Signed Up!</p>}
+      {successMsg && <p className="text-green-700">Successfully Signed Up !</p>}
     </div>
   );
 };
