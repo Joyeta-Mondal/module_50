cbdf23d2bfc1292761717cf66024b8deb8bbf40f
Joyeta-Mondal <dipannitakotha2019@gmail.com>
2025-01-10 16:31:00 +0530

    edited navbar

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,29 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./Layout/Main.jsx";
+import Home from "./components/Home/Home.jsx";
+import Login from "./components/Login/Login.jsx";
+import Register from "./components/Register/Register.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+      {
+        path: "register",
+        element: <Register></Register>,
+      },
+    ],
   },
 ]);
 createRoot(document.getElementById("root")).render(
